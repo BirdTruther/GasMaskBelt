4777af5d217b41826ce126f5bd0d06e0540b3c22
BirdTruther <129489950+BirdTruther@users.noreply.github.com>
2026-07-07 01:06:43 -0400

    Add gas mask hotbar wear/unwear support
    Add hotbar integration for gas mask equipping. When a gas mask is used from the hotbar, it will toggle between equipped and unequipped states. Handles inventory transfers, body location resolution, and UI updates.

---
+++ Contents/mods/GasMaskBelt/42/media/lua/client/Hotbar/gasmask_hotbar_wear.lua
@@ -0,0 +1,46 @@
+require "Hotbar/ISHotbar"
+
+local original_equipItem = ISHotbar.equipItem
+
+local function isItemWorn(character, targetItem)
+    local wornItems = character:getWornItems()
+    for i = 0, wornItems:size() - 1 do
+        local wornItem = wornItems:getItemByIndex(i)
+        if wornItem == targetItem then
+            return true
+        end
+    end
+    return false
+end
+
+local function getItemBodyLocation(item)
+    if item and item.getBodyLocation then
+        return item:getBodyLocation()
+    end
+    return nil
+end
+
+function ISHotbar:equipItem(item)
+    if item and item:getFullType() == "Base.Hat_GasMask" then
+        ISInventoryPaneContextMenu.transferIfNeeded(self.chr, item)
+
+        local bodyLocation = getItemBodyLocation(item)
+        if not bodyLocation then
+            print("GasMask hotbar: failed to resolve body location from getBodyLocation()")
+            return
+        end
+
+        if isItemWorn(self.chr, item) then
+            ISTimedActionQueue.add(ISUnequipAction:new(self.chr, item, 20))
+        else
+            self.chr:setWornItem(bodyLocation, item)
+            triggerEvent("OnClothingUpdated", self.chr)
+        end
+
+        self.chr:getInventory():setDrawDirty(true)
+        getPlayerData(self.chr:getPlayerNum()).playerInventory:refreshBackpacks()
+        return
+    end
+
+    return original_equipItem(self, item)
+end
